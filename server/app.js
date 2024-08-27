2aded7672cdf33a66dd02c183d69fa150288ccd8
xgg0213 <m.eringan@gmail.com>
2024-08-27 15:50:28 -0700

    completed

---
+++ server/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+// Your code here 
+// // part 1:
+// app.use('/stylesheets',express.static('assets/css'));
+
+// // part 2:
+// app.use('/', express.static('assets'));
+
+// part 3:
+app.use('/', express.static('assets/scripts'));
+
+const port = 8000;
+app.listen(port, () => console.log('Server is listening on port', port));
